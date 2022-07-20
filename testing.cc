e879da795b6e2c957f0d5582ae101f8ae6121722
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-07-21 11:38:42 +1200

    Added basic variable examples

---
+++ testing.cc
@@ -0,0 +1,31 @@
+//Welcome to C++ testing!
+//This is a single line comment
+/* This is a 
+multi-line comment
+*/
+
+//Basic Boilerplate
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to C++ testing!";
+    return 0;
+}
+
+//VARIABLES
+
+char character;
+
+int number;
+
+float floatingPointNumber;  
+
+double doublePrecisionNumber;   
+
+void nothing;
+
+bool trueOrFalse;   
+
